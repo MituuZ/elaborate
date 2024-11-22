7003fbc0851b24357af68b1509e48aedbf731380
mitja.leino <mitja.leino@hotmail.com>
2024-11-22 19:30:34 +0200

    Add

--- src/main/java/com/mituuz/elaborate/Elaborate.java
+++ src/main/java/com/mituuz/elaborate/Elaborate.java
@@ -1,13 +1,35 @@
 package com.mituuz.elaborate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Elaborate<T> {
     private final Class<T> analyzeClass;
+    private final List<T> analyzeClasses = new ArrayList<>();
+
+    public void analyze() {
+        for (T instance : analyzeClasses) {
+            System.out.println(instance);
+        }
+    }
 
     public Elaborate(Class<T> analyzeClass) {
         this.analyzeClass = analyzeClass;
     }
 
     public String getClassName() {
-        return analyzeClass.getName();
+        return this.analyzeClass.getName();
+    }
+
+    public void addInstance(T instance) {
+        this.analyzeClasses.add(instance);
+    }
+
+    public void addInstances(List<T> instances) {
+        this.analyzeClasses.addAll(instances);
+    }
+
+    public List<T> getAnalyzeClasses() {
+        return this.analyzeClasses;
     }
 }
--- src/test/java/com/mituuz/elaborate/ElaborateTest.java
+++ src/test/java/com/mituuz/elaborate/ElaborateTest.java
@@ -1,15 +1,32 @@
 package com.mituuz.elaborate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ElaborateTest {
+    @BeforeEach
+    void setUp() {
+    }
+
     @Test
-    void check() {
-        Elaborate<String> elaborate = new Elaborate<>(String.class);
+    void checkClassName() {
+        var elaborate = new Elaborate<>(String.class);
         String className = elaborate.getClassName();
         assertEquals("java.lang.String", className);
+    }
+
+    @Test
+    void addInstances() {
+        var elaborate = new Elaborate<>(String.class);
+        elaborate.addInstance("Hello");
+        elaborate.addInstance("World");
+        assertEquals(2, elaborate.getAnalyzeClasses().size());
+        elaborate.addInstances(List.of("Hello", "World"));
+        assertEquals(4, elaborate.getAnalyzeClasses().size());
     }
 }
